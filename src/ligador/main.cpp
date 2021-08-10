bb5813c167f5d40e2eefb4b0e9ab48ca819a5413
TalesPanoutsos <tales.panoutsos@gmail.com>
2021-08-10 15:28:10 -0300

    init

--- src/ligador/main.cpp
+++ src/ligador/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
-
+#include <list>
+#include <map>
 #include "ligador.h"
+using namespace std;
+
+
+bool is_number(string s){
+    for (int i = 0; i < s.size(); ++i)
+        if(not is_digit(s[i]))
+            return false;
+    return true;
+}
+
+
+list<string>* link(list<*list<string>>* programs, list<*map<string, int>>* dicts){
+    list<string>* linked_program;
+
+
+
+
+    return linked_program
+}
+
 
 int main(int argc, char *argv[]) {
     // TODO: executar o ligador...
-    std::cout << "Hello World" << std::endl;
+    cout << "Hello World" << endl;
+
+    
     return 0;
 }
