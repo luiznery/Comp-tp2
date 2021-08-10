b73b0def59763a7f1593c0424a6b3687a0f62485
Pedro <0wfonsecapedro@gmail.com>
2021-08-10 17:44:25 -0300

    input

--- src/ligador/ligador.cpp
+++ src/ligador/ligador.cpp
@@ -1,3 +1,24 @@
 #include "ligador.h"
 
 // TODO: implementação do editor de ligação
+
+tuple<map<string,int>*,list<string>*> build(list<string>* data){
+    list<string>* row = new list<string>;
+
+    map<string,int>* dicio = new map<string,int>;
+    list<string>* program = new list<string>;
+
+    for(auto it = data->begin(); it != data->end(); ++it)
+    {
+        if (it->size() == 0){
+            it++;
+            program = split(*it,' ');
+        }
+        else{
+            row = split(*it,' ');
+            dicio->insert(pair<string, int>(*get_element(row,0), stoi(*get_element(row,1))));
+        }
+    }
+    return make_tuple(dicio,program);
+}
+
--- src/ligador/utils_ligador.cpp
+++ src/ligador/utils_ligador.cpp
@@ -1,8 +1,74 @@
+#include <list>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
 
 #include "utils_ligador.h"
 
 using namespace std;
+
+list<string>* read_input(string file_name) {
+    string line;
+    ifstream myfile;
+
+    myfile.open(file_name);
+    if(!myfile.is_open()) {
+        perror("Error open");
+        exit(EXIT_FAILURE);
+    }
+
+    list<string> *lines = new list<string>;
+    while(getline(myfile, line)) {
+        lines->push_back(line);
+    }
+
+    return lines;
+}
+
+list<string>* split(string text, char separator){
+    list<string>* word_list = new list<string>();
+
+    string word = "";
+    for(unsigned i = 0; i < text.size(); i++){
+        if(text[i]!= separator){
+            word = word + text[i];
+        }else{
+            if(word!="")
+                word_list->push_back(word);
+            word = "";
+        }
+
+    }
+    if(word!="")
+        word_list->push_back(word);
+
+    return word_list;
+}
+
+list<string>::iterator get_element(list<string>* lista, int position) {
+    int c = 0;
+    for(auto it = lista->begin(); it!=lista->end(); it++){
+        if (c==position)
+            return it;
+        c++;
+    }
+    return lista->end();
+}
+
+void print_list(list<string>* lista, string sep, string end){
+    for(auto it = lista->begin(); it!=(prev(lista->end())); it++){
+        cout << *it << sep;
+    }
+    cout << *(prev(lista->end()));
+    cout << end;
+}
+
+void print_map(map<string,int>* m) {
+    for (const auto& x : (*m)) {
+        std::cout << x.first << ": " << x.second << "\n";
+    }
+}
 
 void teste() {
     cout<<"teste"<<endl;
